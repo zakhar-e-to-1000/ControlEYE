8d38d8e1b6e16c6534b33daf048401a551fe258f
Zakhar <super.korustyvach@gmail.com>
2024-12-16 18:16:27 +0200

    Quick fix: fix video shrinking during high quality mode

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Repository with code of ControlEYE TEAM (LPNU univesity)
+
+## camera.py
+Python script for computer that uses ffmpeg library and CameraWebserver.ino expample sketch from Arduino esp32 library to take footage.
--- camera.py
+++ camera.py
@@ -4,11 +4,9 @@ Camera operator program v0.0.1
 import os
 import requests
 import time
-import threading
-from functools import partial
 
 def get_video(IP, OUTPUT_PATH):
-    os.system(f'ffmpeg -i "{IP}:81/stream" -t 20 -c copy "{OUTPUT_PATH}.mkv"')
+    os.system(f'ffmpeg -use_wallclock_as_timestamps 1 -i "{IP}:81/stream" -t 60 -c copy "{OUTPUT_PATH}.mp4"')
 
 def change_quality(IP):
     time.sleep(5)
@@ -23,8 +21,4 @@ print("Please enter path for video output")
 OUTPUT_PATH = input()
 requests.get(f"{IP}/control?var=framesize&val=2")
 
-chore1 = threading.Thread(target=partial(get_video, IP, OUTPUT_PATH))
-chore2 = threading.Thread(target=partial(change_quality, IP))
-chore2.start()
-chore1.start()
-#os.system(f'ffmpeg -i "{IP}:81/stream" -t 20 -c copy "{OUTPUT_PATH}2.mkv"')
+get_video(IP, OUTPUT_PATH)
